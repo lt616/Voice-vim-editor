d510e9e730adc3e5d500b5f6eb2e2df53c055525
肇小狼 <qz2338@columbia.edu>
2019-04-22 21:19:48 -0400

    MODIFY libclang-vim

---
+++ voice-plugin/stringizers.hpp
@@ -0,0 +1,48 @@
+#if !defined LIBCLANG_VIM_STRINGIZERS_HPP_INCLUDED
+#define LIBCLANG_VIM_STRINGIZERS_HPP_INCLUDED
+
+#include <string>
+
+#include <clang-c/Index.h>
+
+#include "helpers.hpp"
+
+namespace libclang_vim {
+
+std::string stringize_spell(CXCursor const& cursor);
+
+std::string stringize_extra_type_info(CXType const& type);
+
+std::string stringize_type(CXType const& type);
+
+std::string stringize_linkage_kind(CXLinkageKind const& linkage);
+
+std::string stringize_linkage(CXCursor const& cursor);
+
+std::string stringize_parent(CXCursor const& cursor, CXCursor const& parent);
+
+std::string stringize_location(CXSourceLocation const& location);
+
+std::string stringize_cursor_location(CXCursor const& cursor);
+
+std::string stringize_cursor_kind_type(CXCursorKind const& kind);
+
+std::string stringize_cursor_extra_info(CXCursor const& cursor);
+
+std::string stringize_cursor_kind(CXCursor const& cursor);
+
+std::string stringize_included_file(CXCursor const& cursor);
+
+std::string stringize_cursor(CXCursor const& cursor, CXCursor const& parent);
+
+std::string stringize_range(CXSourceRange const& range);
+
+std::string stringize_end(CXCursor const& currsor);
+
+std::string stringize_extent(CXCursor const& cursor);
+
+} // namespace libclang_vim
+
+#endif // LIBCLANG_VIM_STRINGIZERS_HPP_INCLUDED
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
